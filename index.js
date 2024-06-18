bfd1026a0ae7bac3245bd98def831e833fbc9a32
G0KU0 <43786938+G0KU0@users.noreply.github.com>
2024-06-18 16:12:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730];  // Enter here AppIDs of the needed games
+var games = [730,578080,2281730];  // Enter here AppIDs of the needed games
 var status = 7;  // 1 - online, 7 - invisible
 
 
